8a6b74f31cfa588894e6f78a086d4fa0ff9ef2ae
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-21 14:35:56 -0300

    Class05 - Atv8

--- app/models/negotiation.ts
+++ app/models/negotiation.ts
@@ -1,11 +1,16 @@
 export class Negotiation {
   constructor(
-    public readonly date: Date,
+    private _date: Date,
     public readonly amount: number,
     public readonly value: number
   ) {}
 
   get volume(): number {
     return this.amount * this.value;
+  }
+
+  get date(): Date {
+    const date = new Date(this._date.getTime());
+    return date;
   }
 }
