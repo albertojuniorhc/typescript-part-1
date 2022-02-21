618ccafb081500d1873b733b6381d06ff4077e19
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-21 13:14:45 -0300

    Class04 - Atv5

--- app/models/negotiations.ts
+++ app/models/negotiations.ts
@@ -1,5 +1,13 @@
 import { Negotiation } from "./negotiation.js";
 
 export class Negotiations {
-    private negotiations: Array<Negotiation> = [];
+  private negotiations: Array<Negotiation> = [];
+
+  add(negotiation: Negotiation) {
+    this.negotiations.push(negotiation);
+  }
+
+  list(): Array<Negotiation>{
+    return this.negotiations;
+  }
 }
